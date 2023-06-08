0e550e9da8a0430d8844c52eab56cb8b241c3d15
Bernard GoleCruz <golecruz@unlv.nevada.edu>
2023-06-07 20:14:38 -0700

    first commit

---
+++ demo/src/main/java/DAO/UserDAO.java
@@ -0,0 +1,8 @@
+package DAO;
+import Model.User;
+import Model.Client;
+import Model.Vistor;
+
+public class UserDAO {
+    
+}
---
+++ demo/src/main/java/Model/Client.java
@@ -0,0 +1,6 @@
+package Model;
+
+public class Client extends User {
+    
+    
+}
