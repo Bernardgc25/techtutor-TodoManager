8eedd404f6ae7d9df011537813a3799e189a28cf
Bernard GoleCruz <golecruz@unlv.nevada.edu>
2023-06-08 19:24:09 -0700

    implement UserDAOimpl class

--- demo/src/main/java/DAOclass/UserDAOimpl.java
+++ demo/src/main/java/DAOclass/UserDAOimpl.java
@@ -1,5 +1,7 @@
 package DAOclass;
 
+import java.util.Scanner;
+
 import DAOinterface.UserDAO;
 
 import Model.User;
@@ -7,17 +9,65 @@ import Model.Client;
 import Model.Vistor;
 
 public class UserDAOimpl implements UserDAO {
+    User u = new User(); 
+    String firstName,lastName,userName,password;  
 
     @Override
     public void register() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'register'");
+        //throw new UnsupportedOperationException("Unimplemented method 'register'");
+        
+           try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter firstName: ");
+            firstName = scanner.nextLine();
+            //register.setFirstName(firstName);
+            u.setFirstname(firstName);
+
+            System.out.print("Enter lastName: ");
+            lastName = scanner.nextLine();
+            //register.setLastName(lastName);
+            u.setLastname(lastName);
+
+            System.out.print("Enter userName: ");
+            userName = scanner.nextLine();
+            //register.setUserName(userName);
+            u.setUsername(userName);
+
+            System.out.print("Enter password: ");
+            password = scanner.nextLine();
+            //register.setPassword(password);
+            u.setPassword(password);
+
+        
+        }
     }
+    
 
     @Override
     public void login() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        //throw new UnsupportedOperationException("Unimplemented method 'login'");
+          try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print(" Enter user name: ");
+            userName = scanner.nextLine();
+
+            System.out.print(" Enter password: ");
+            password = scanner.nextLine();
+
+            /* 
+            if ("ramesh".equals(userName) && "password".equals(password)) {
+                System.out.println(" User successfully logged-in.. ");
+            } else {
+                System.out.println(" In valid userName of password ");
+            }  
+            */
+            if(userName.equals(u.getUsername()) && password.equals(u.getPassword())){
+                System.out.println("User successfully logged-in..");
+            }            
+            else{
+                System.out.println("Invalid try again");
+            }
+
+        }
+    
     }
     
 }
